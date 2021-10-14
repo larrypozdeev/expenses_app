5be5f38215f2f733c1892687d1dcf7f85981ba69
Dmitry Pozdeev <dmitry_pozdeev@icloud.com>
2021-10-14 16:28:22 -0700

    init tests

--- run.py
+++ run.py
@@ -17,3 +17,12 @@ def make_shell_context():
         Expenses=Expenses,
         Balances=Balances,
     )
+
+
+@app.cli.command()
+def test():
+    """Run the unit tests."""
+    import unittest
+
+    tests = unittest.TestLoader().discover("tests")
+    unittest.TextTestRunner(verbosity=2).run(tests)
